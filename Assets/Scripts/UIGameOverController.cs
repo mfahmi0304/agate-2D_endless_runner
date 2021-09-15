48b1438c3b5952070fe83ec2ff629dfc29c33c1f
unknown <m.fahminuraziz93@gmail.com>
2021-09-15 19:22:43 +0700

    [UPDATE] Build

--- Assets/Scripts/UIGameOverController.cs
+++ Assets/Scripts/UIGameOverController.cs
@@ -5,10 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class UIGameOverController : MonoBehaviour
 {
+    public AudioSource AudioGameOver;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        AudioGameOver.Play();
     }
 
     // Update is called once per frame
